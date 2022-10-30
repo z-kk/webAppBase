8fbeb8dd2966f4ce66b56d8d90c98d7c9d8632bc
z-kk <z-kk@gmail.com>
2022-10-30 19:47:13 +0900

    get port from cmdline

--- src/webAppBase.nim
+++ src/webAppBase.nim
@@ -1,6 +1,9 @@
 import
-  std / [os, random],
+  std / [os, strutils, random],
   webAppBasepkg / [webserver, dbtables]
+
+const
+  DefaultPort = 5000
 
 proc createConfDir() =
   let dir = getConfigDir() / getAppFilename().extractFilename
@@ -12,7 +15,17 @@ proc createDb() =
     let db = openDb()
     db.createTables
 
+proc getPort(): int =
+  if commandLineParams().len == 0:
+    DefaultPort
+  else:
+    try:
+      commandLineParams()[0].parseInt
+    except:
+      echo "usage: $1 [Port]" % [getAppFilename().extractFilename]
+      quit()
+
 when isMainModule:
   randomize()
   createDb()
-  startWebServer()
+  startWebServer(getPort())
