33d9d9ba7eaefc2b7acaad1b64989515f9975ba6
z-kk <z-kk@gmail.com>
2022-11-16 18:29:56 +0900

    change get port from cmdline proc

--- src/webAppBase.nim
+++ src/webAppBase.nim
@@ -1,6 +1,10 @@
 import
-  std / [os, strutils, random],
+  std / [os, strutils, parseopt, random],
   webAppBasepkg / [webserver, dbtables]
+
+type
+  CmdOpt = object
+    port: int
 
 const
   DefaultPort = 5000
@@ -19,16 +23,53 @@ proc createDb() =
     defer: db.close
     db.createTables
 
-proc getPort(): int =
-  if commandLineParams().len == 0:
-    DefaultPort
-  else:
-    try:
-      commandLineParams()[0].parseInt
-    except:
-      quit("usage: $1 [Port]" % [appName])
+proc readCmdOpt(): CmdOpt =
+  ## Read command line options.
+  var
+    p = initOptParser()
+    fail = false
+  while true:
+    p.next
+    case p.kind
+    of cmdArgument:
+      if result.port == 0:
+        try:
+          result.port = p.key.parseInt
+        except:
+          fail = true
+          break
+      else:
+        fail = true
+        break
+    of cmdLongOption, cmdShortOption:
+      case p.key
+      of "p", "port":
+        var val = p.val
+        if val == "":
+          p.next
+          if p.kind != cmdArgument:
+            fail = true
+            break
+          val = p.key
+        try:
+          result.port = val.parseInt
+        except:
+          fail = true
+          break
+      else:
+        fail = true
+        break
+    of cmdEnd:
+      break
+
+  if fail:
+    quit("usage: $1 [[-p|--port] Port]" % [appName])
+
+  if result.port == 0:
+    result.port = DefaultPort
 
 when isMainModule:
   randomize()
   createDb()
-  startWebServer(getPort(), "/" & appName)
+  let cmdOpt = readCmdOpt()
+  startWebServer(cmdOpt.port, "/" & appName)
